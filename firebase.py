fc768900771c053180a733d3eab56ad54e4ce78a
Emma Wang <eyw27@cornell.edu>
2025-04-01 00:55:54 -0400

    Encode service account instead of using filepath

--- firebase.py
+++ firebase.py
@@ -2,9 +2,18 @@
 import firebase_admin
 from firebase_admin import credentials
 import os
-from dotenv import load_dotenv
+import base64
+import json
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-cred = credentials.Certificate("FIREBASE_SERVICE_ACCOUNT")
+firebase_json_b64 = os.getenv("FIREBASE_ENCODED_CREDENTIALS")
+# Decode credentials back to JSON
+firebase_json = base64.b64decode(firebase_json_b64).decode("utf-8")
+# Convert to dictionary and use it directly
+firebase_dict = json.loads(firebase_json)
+
+cred = credentials.Certificate(firebase_dict)
+# cred = credentials.Certificate("FIREBASE_SERVICE_ACCOUNT")
 firebase_app=firebase_admin.initialize_app(cred)
