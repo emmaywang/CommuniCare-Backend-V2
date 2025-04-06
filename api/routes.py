c8761ef69f5151f4651a12c1bcbd0f30f715b10e
Emma Wang <eyw27@cornell.edu>
2025-04-06 10:08:08 -0400

    Edit bookmark routes to match API and database
    -also temporarily commented out Reminders routes

--- api/routes.py
+++ api/routes.py
@@ -12,7 +12,7 @@ from math import radians, sin, cos, sqrt, atan2
 from dotenv import load_dotenv
 import os
 
-
+# Change these values to the ones used to create the App Service.
 
 app = Flask(__name__)
 
@@ -594,7 +594,7 @@ def update_user_premium(username):
 
 #------BOOKMARKS---------
 
-@app.route('/bookmark-lists', methods=['POST'])
+@app.route('/api/bookmark-lists', methods=['POST'])
 def create_bookmark_list():
     data = request.get_json()
     username = data.get('username')
@@ -610,7 +610,7 @@ def create_bookmark_list():
         cursor.execute("""
             INSERT INTO BookmarkLists (username, list_name)
             VALUES (?, ?)
-        """, (username, list_name))
+        """, (username, list_name)) #database automatically adds list_id and created_at
         conn.commit()
         return jsonify({"message": "Bookmark list created successfully"}), 201
     except Exception as e:
@@ -621,24 +621,25 @@ def create_bookmark_list():
         
         
         
-@app.route('/bookmark-lists/<int:list_id>/bookmarks', methods=['POST'])
+@app.route('/api/bookmark-lists/<int:list_id>/bookmarks', methods=['POST'])
 def add_bookmark(list_id):
     data = request.get_json()
     resource_type = data.get('resource_type')
     resource_id = data.get('resource_id')
     note = data.get('note', '')
-    
+    protected_route()
     if not resource_type or not resource_id:
         return jsonify({"error": "Missing resource_type or resource_id"}), 400
 
     conn = get_db_connection()
     cursor = conn.cursor()
-    username_check(username)
+
     try:
         cursor.execute("""
             INSERT INTO Bookmarks (list_id, resource_type, resource_id, note)
             VALUES (?, ?, ?, ?)
-        """, (list_id, resource_type, resource_id, note))
+        """, (list_id, resource_type, resource_id, note)) #database automatically adds list_id and created_at
+        #list_id must already exist (so entry in BookmarkLists must be created first)
         conn.commit()
         return jsonify({"message": "Bookmark added successfully"}), 201
     except Exception as e:
@@ -648,7 +649,7 @@ def add_bookmark(list_id):
         conn.close()
 
 
-@app.route('/users/<username>/bookmark-lists', methods=['GET'])
+@app.route('/api/users/<username>/bookmark-lists', methods=['GET'])
 def get_user_bookmark_lists(username):
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -695,11 +696,11 @@ def get_user_bookmark_lists(username):
         conn.close()
 
 
-@app.route('/bookmark-lists/<int:list_id>/bookmarks/<int:bookmark_id>', methods=['DELETE'])
+@app.route('/api/bookmark-lists/<int:list_id>/bookmarks/<int:bookmark_id>', methods=['DELETE'])
 def delete_bookmark(list_id, bookmark_id):
     conn = get_db_connection()
     cursor = conn.cursor()
-    username_check(username)
+
     try:
         cursor.execute("""
             DELETE FROM Bookmarks
@@ -721,112 +722,112 @@ def delete_bookmark(list_id, bookmark_id):
         
 # --------REMINDERS--------------
 
-# Get all reminders of user by username. 
-# When authorization is integrated maybe we require User as parameter instead.
-@app.route('/reminder/<username>', methods=['GET'])
-def get_reminders_by_user(user_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Users WHERE user_id = " + user_id)
-    if not cursor.fetchall():
-        return jsonify({'message': 'User not found'}), 404
-    cursor.execute("SELECT clinic, program, time, subject FROM Appointments WHERE user_id = ?", (user_id,))
-    reminders = [
-        {"clinic_id": row[0], "program": row[1], "time": row[2], "subject": row[3]}
-        for row in cursor.fetchall()
-    ]
-    cursor.close()
-    conn.close()
-    return jsonify(reminders), 200
+# # Get all reminders of user by username. 
+# # When authorization is integrated maybe we require User as parameter instead.
+# @app.route('/reminder/<username>', methods=['GET'])
+# def get_reminders_by_user(user_id: int):
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM Users WHERE user_id = " + user_id)
+#     if not cursor.fetchall():
+#         return jsonify({'message': 'User not found'}), 404
+#     cursor.execute("SELECT clinic, program, time, subject FROM Appointments WHERE user_id = ?", (user_id,))
+#     reminders = [
+#         {"clinic_id": row[0], "program": row[1], "time": row[2], "subject": row[3]}
+#         for row in cursor.fetchall()
+#     ]
+#     cursor.close()
+#     conn.close()
+#     return jsonify(reminders), 200
 
-# helper for getting one reminder by clinic, user and program.
-@app.route('/reminder/<user_id>/<clinic_id>', methods=['GET'])
-def get_reminder_by_clinic_user(user_id: int, clinic_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
+# # helper for getting one reminder by clinic, user and program.
+# @app.route('/reminder/<user_id>/<clinic_id>', methods=['GET'])
+# def get_reminder_by_clinic_user(user_id: int, clinic_id: int):
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
     
-    cursor.execute("SELECT 1 FROM Users WHERE user_id = ?", (user_id,))
-    if not cursor.fetchone():
-        return jsonify({'message': 'User not found'}), 404
-    reminder = get_reminder_helper(cursor, user_id, clinic_id)
-    if not reminder:
-        return jsonify({'message': 'Appointment not found.'}), 404
-    return jsonify(reminder), 201
+#     cursor.execute("SELECT 1 FROM Users WHERE user_id = ?", (user_id,))
+#     if not cursor.fetchone():
+#         return jsonify({'message': 'User not found'}), 404
+#     reminder = get_reminder_helper(cursor, user_id, clinic_id)
+#     if not reminder:
+#         return jsonify({'message': 'Appointment not found.'}), 404
+#     return jsonify(reminder), 201
     
-def get_reminder_helper(cursor, user_id: int, clinic_id: int):
-    cursor.execute(
-        "SELECT clinic_id, program, time, subject FROM Appointments "
-        "WHERE user_id = ? AND clinic_id = ?", 
-        (user_id, clinic_id)
-    )
-    row = cursor.fetchone()
-    if not row:
-        return None
-    reminder = {
-        "clinic_id": row.clinic_id, "program": row.program, "time": row.time, "subject": row.subject
-    }
-    return reminder
+# def get_reminder_helper(cursor, user_id: int, clinic_id: int):
+#     cursor.execute(
+#         "SELECT clinic_id, program, time, subject FROM Appointments "
+#         "WHERE user_id = ? AND clinic_id = ?", 
+#         (user_id, clinic_id)
+#     )
+#     row = cursor.fetchone()
+#     if not row:
+#         return None
+#     reminder = {
+#         "clinic_id": row.clinic_id, "program": row.program, "time": row.time, "subject": row.subject
+#     }
+#     return reminder
 
-@app.route('/reminder/<user_id>/<clinic_id>', methods=['PUT'])
-def update_reminder_by_clinic(user_id: int, clinic_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Users WHERE user_id = ?", (user_id,))
-    if not cursor.fetchall():
-        return jsonify({'message': 'User not found'}), 404
-    newInfo = request.get_json()
-    if 'program' not in newInfo or 'time' not in newInfo or 'subject' not in newInfo:
-        return jsonify({"message": "incomplete information in request"}), 400
-    cursor.execute("UPDATE Appointments SET program = ?, time = ?, subject = ? " +
-                   "WHERE user_id = ? AND clinic_id = ?",
-                   (newInfo['program'], newInfo['time'], newInfo['subject'], user_id, clinic_id))
-    conn.commit()
+# @app.route('/reminder/<user_id>/<clinic_id>', methods=['PUT'])
+# def update_reminder_by_clinic(user_id: int, clinic_id: int):
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM Users WHERE user_id = ?", (user_id,))
+#     if not cursor.fetchall():
+#         return jsonify({'message': 'User not found'}), 404
+#     newInfo = request.get_json()
+#     if 'program' not in newInfo or 'time' not in newInfo or 'subject' not in newInfo:
+#         return jsonify({"message": "incomplete information in request"}), 400
+#     cursor.execute("UPDATE Appointments SET program = ?, time = ?, subject = ? " +
+#                    "WHERE user_id = ? AND clinic_id = ?",
+#                    (newInfo['program'], newInfo['time'], newInfo['subject'], user_id, clinic_id))
+#     conn.commit()
     
-    reminder = get_reminder_helper(cursor, user_id, clinic_id)
-    if not reminder:
-        return jsonify({'message': 'Appointment failed to update'}), 500
-    return jsonify(reminder), 201
+#     reminder = get_reminder_helper(cursor, user_id, clinic_id)
+#     if not reminder:
+#         return jsonify({'message': 'Appointment failed to update'}), 500
+#     return jsonify(reminder), 201
 
-@app.route('/reminder/<user_id>', methods=['POST'])
-def create_reminder_by_user(user_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Users WHERE user_id = ?", (user_id,))
-    if not cursor.fetchall():
-        return jsonify({'message': 'User not found'}), 404
-    newInfo = request.get_json()
-    if not all(key in newInfo for key in ['clinic_name', 'program', 'time', 'subject']):
-        return jsonify({"message": "incomplete information in request"}), 400
-    cursor.execute("SELECT clinic_id FROM Clinics WHERE clinic_name = ?", (newInfo['clinic_name'],))
-    clinicId = cursor.fetchone()
-    if not clinicId:
-        return jsonify({'message': 'Clinic of create reminder request not found'}), 404
-    cursor.execute("INSERT INTO Appointments (user_id, clinic_id, program, time, subject) VALUES (?,?,?,?,?)",
-                   (user_id, clinicId, newInfo['program'], newInfo['time'], newInfo['subject']))
-    conn.commit()
+# @app.route('/reminder/<user_id>', methods=['POST'])
+# def create_reminder_by_user(user_id: int):
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM Users WHERE user_id = ?", (user_id,))
+#     if not cursor.fetchall():
+#         return jsonify({'message': 'User not found'}), 404
+#     newInfo = request.get_json()
+#     if not all(key in newInfo for key in ['clinic_name', 'program', 'time', 'subject']):
+#         return jsonify({"message": "incomplete information in request"}), 400
+#     cursor.execute("SELECT clinic_id FROM Clinics WHERE clinic_name = ?", (newInfo['clinic_name'],))
+#     clinicId = cursor.fetchone()
+#     if not clinicId:
+#         return jsonify({'message': 'Clinic of create reminder request not found'}), 404
+#     cursor.execute("INSERT INTO Appointments (user_id, clinic_id, program, time, subject) VALUES (?,?,?,?,?)",
+#                    (user_id, clinicId, newInfo['program'], newInfo['time'], newInfo['subject']))
+#     conn.commit()
     
-    reminder = get_reminder_helper(cursor, user_id, clinicId)
-    if not reminder:
-        return jsonify({'message': 'Appointment failed to update'}), 500
-    return jsonify(reminder), 201
+#     reminder = get_reminder_helper(cursor, user_id, clinicId)
+#     if not reminder:
+#         return jsonify({'message': 'Appointment failed to update'}), 500
+#     return jsonify(reminder), 201
 
-@app.route('/reminder/<user_id>', methods=['DELETE'])
-def delete_reminder_by_user_clinic(user_id: int, clinic_id: int):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Users WHERE user_id = ?", (user_id,))
-    if not cursor.fetchall():
-        return jsonify({'message': 'User not found'}), 404
-    reminder = get_reminder_helper(cursor, user_id, clinic_id)
-    if not reminder:
-        return jsonify({'message': 'Appointment not found.'}), 404
-    cursor.execute("DELETE FROM Appointments WHERE user_id = ? AND clinic_id = ?", 
-        (user_id, clinic_id))
-    reminder = get_reminder_helper(cursor, user_id, clinic_id)
-    if not reminder:
-        return jsonify({'message': 'Appointment deleted.'}), 201
-    else:
-        return jsonify({'message': 'Appointment failed to delete.'}), 500
+# @app.route('/reminder/<user_id>', methods=['DELETE'])
+# def delete_reminder_by_user_clinic(user_id: int, clinic_id: int):
+#     conn = get_db_connection()
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT * FROM Users WHERE user_id = ?", (user_id,))
+#     if not cursor.fetchall():
+#         return jsonify({'message': 'User not found'}), 404
+#     reminder = get_reminder_helper(cursor, user_id, clinic_id)
+#     if not reminder:
+#         return jsonify({'message': 'Appointment not found.'}), 404
+#     cursor.execute("DELETE FROM Appointments WHERE user_id = ? AND clinic_id = ?", 
+#         (user_id, clinic_id))
+#     reminder = get_reminder_helper(cursor, user_id, clinic_id)
+#     if not reminder:
+#         return jsonify({'message': 'Appointment deleted.'}), 201
+#     else:
+#         return jsonify({'message': 'Appointment failed to delete.'}), 500
 
 
 if __name__ == '__main__':
